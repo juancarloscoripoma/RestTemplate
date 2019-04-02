1ad7cc1ec95a8a9f09d73be812a845c4f6e13365
jcarlos <jcori.transoft@gmail.com>
2019-04-02 18:29:18 -0400

    modified version

--- rtemplate/src/main/java/com/soft/rtemplate/web/rest/RTemplateResource.java
+++ rtemplate/src/main/java/com/soft/rtemplate/web/rest/RTemplateResource.java
@@ -3,15 +3,14 @@ package com.soft.rtemplate.web.rest;
 import com.soft.rtemplate.service.dto.UserDTO;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.*;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 import java.math.BigDecimal;
+import java.util.Arrays;
 import java.util.Map;
 
 @RestController
@@ -27,26 +26,25 @@ public class RTemplateResource {
 
     @GetMapping(value = "/demo")
     public String getIndex() {
-        String url = "http://localhost:8202/api/enlace";
-// https://howtodoinjava.com/spring-restful/spring-restful-client-resttemplate-example/
-
+        String url = "http://localhost:8203/api/enlace";
         RestTemplate restTemplate = new RestTemplate();
 
-        //ResponseEntity<UserDTO> greeting = restTemplate.getForEntity(url, UserDTO.class);
-        // LOGGER.info(greeting.getBody().getValue());
-
-
         UserDTO userDTO = new UserDTO();
-        userDTO.setAmount(new BigDecimal(15));
+        userDTO.setAmount(new BigDecimal(5));
         userDTO.setCardCode("1984-11");
         userDTO.setPassword("Password1!");
         userDTO.setStatus(Boolean.FALSE);
         userDTO.setUser("ston");
 
-        //myGreeting.setValue("Hey ho!");
-        //HttpEntity<UserDTO> entity = new HttpEntity<UserDTO>(userDTO);
-        //restTemplate.exchange(url, HttpMethod.PUT, entity, UserDTO.class);
-        restTemplate.put(url,userDTO);
+        HttpHeaders headers = new HttpHeaders();
+
+        headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
+        headers.setContentType(MediaType.APPLICATION_JSON);
+
+        HttpEntity<UserDTO> requestEntity = new HttpEntity<UserDTO>(userDTO, headers);
+        ResponseEntity<UserDTO> uri = restTemplate.exchange(url, HttpMethod.POST, requestEntity, UserDTO.class);
+
+        System.out.println(uri.getBody());
 
         return "hola";
     }
