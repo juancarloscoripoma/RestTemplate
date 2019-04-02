9025debc60bf550d58fce68b2419a4ee848b31f8
carlos <inf.juan.carlos@gmail.com>
2019-04-02 07:23:05 -0800

    rest template for put

--- rtemplate/src/main/java/com/soft/rtemplate/web/rest/RTemplateResource.java
+++ rtemplate/src/main/java/com/soft/rtemplate/web/rest/RTemplateResource.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 import java.math.BigDecimal;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api")
@@ -21,27 +22,31 @@ public class RTemplateResource {
 
     private static final String ENTITY_NAME = "rTemplate";
 
+    private RestTemplate restTemplate;
+    Map<String, String> uriParams;
+
     @GetMapping(value = "/demo")
     public String getIndex() {
-        String url = "http://localhost:8202/api/enlace";
-
+        String url = "http://200.58.78.55:8202/api/enlace";
+// https://howtodoinjava.com/spring-restful/spring-restful-client-resttemplate-example/
 
         RestTemplate restTemplate = new RestTemplate();
 
-        ResponseEntity<UserDTO> greeting = restTemplate.getForEntity(url, UserDTO.class);
+        //ResponseEntity<UserDTO> greeting = restTemplate.getForEntity(url, UserDTO.class);
         // LOGGER.info(greeting.getBody().getValue());
 
 
         UserDTO userDTO = new UserDTO();
-        userDTO.setAmount(new BigDecimal(10));
+        userDTO.setAmount(new BigDecimal(15));
         userDTO.setCardCode("1984-11");
         userDTO.setPassword("Password1!");
         userDTO.setStatus(Boolean.FALSE);
         userDTO.setUser("ston");
 
         //myGreeting.setValue("Hey ho!");
-        HttpEntity<UserDTO> entity = new HttpEntity<UserDTO>(userDTO);
-        restTemplate.exchange(url, HttpMethod.PUT, entity, UserDTO.class);
+        //HttpEntity<UserDTO> entity = new HttpEntity<UserDTO>(userDTO);
+        //restTemplate.exchange(url, HttpMethod.PUT, entity, UserDTO.class);
+        restTemplate.put(url,userDTO);
 
         return "hola";
     }
